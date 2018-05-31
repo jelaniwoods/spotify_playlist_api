d874e4e537f777c010ad37c5266cb3f0242fb4c9
Windows Letgoner <jmw686@nyu.edu>
2018-05-31 17:01:25 -0500

    AYYY

--- client/src/components/Api.js
+++ client/src/components/Api.js
@@ -5,25 +5,53 @@ import {
     Redirect,
     Switch,
 } from 'react-router-dom';
-let spotify_api;
+import Spotify from 'spotify-web-api-js';
+
+const spotify_api = new Spotify();
 
 class Api extends Component {
   constructor(props) {
-      console.log(props);
+    // console.log(props.id);
+      console.log('iwi ');
+      console.log(props.match.params);
     super(props);
     this.state = {
-      tracks: [...this.props.tracks]
+      id: this.props.match.params.user,
+      token: this.props.match.params.token,
+      playlist: this.props.match.params.id,
+      tracks: []
     }
-    spotify_api = this.props.Spotify;
+    spotify_api.setAccessToken(this.props.match.params.token);
+    console.log('LOGGED IN');
+  }
+  getTracks() {
+    console.log(this.state.id);
+    console.log(this.state.playlist);
+    spotify_api.getPlaylistTracks(this.state.id, this.state.playlist)
+      .then((response) => {
+        let temp = [];
+        console.log(response);
+        for (let i = 0; i < response.items.length; i++) {
+          const el = response.items[i];
+          let o = {name: el.track.name, artist: el.track.artists[0].name}
+          // temp.push(el.track.name + ': ' + el.track.artists[0].name);
+          temp.push(o);
+        }
+        this.setState({
+          tracks: [...temp],
+        })
+      })
   }
   render() {
     return (
-      <div className="Api">
-        <ul>
-            {this.state.tracks.map(function(name, index) {
-            return <li key={ index }>x {name} </li>
+      <div className="API">
+        {'{'}
+        <ul onClick={this.getTracks()}>
+            {this.state.tracks.map(function(o, index) {
+            return <li key={ index }> {'{'} name : {o.name}, artist: {o.artist} {'}'},</li>
             })}
         </ul>
+        {'}'}
       </div>
     );
   }
